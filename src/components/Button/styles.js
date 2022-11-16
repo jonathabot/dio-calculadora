68ac9c5a4ccac54aeef562cddff6666737b786ff
Jonatha <lebotelhon@gmail.com>
2022-11-16 09:58:59 -0300

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+  padding: 20px;
+  border: 1px solid #cdcdcd;
+  background-color: #00aaf0;
+  color: #ffffff;
+  font-size: 24px;
+  font-weight: 700;
+  flex: 1;
+  border-radius: 25%;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`;
